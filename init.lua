deb718eb02dfb899c22f8678ea98a67b35679e69
Oliveira <g.oliveira@cfp.energy>
2025-03-08 08:41:44 -0300

    add poetry support for pyright

--- init.lua
+++ init.lua
@@ -637,11 +637,10 @@ require('lazy').setup({
         -- clangd = {},
         -- gopls = {},
         pyright = {
-          settings = {
-            python = {
-              pythonPath = 'python',
-            },
-          },
+          before_init = function(params, config)
+            local python_path = vim.fn.system 'poetry run which python'
+            config.settings.python.pythonPath = vim.trim(python_path)
+          end,
         },
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
